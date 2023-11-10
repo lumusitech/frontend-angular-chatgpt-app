5135e2c2e8ab244f63ef5a360d0c99dd629cb64c
lumusitech <lumusitech@gmail.com>
2023-11-10 00:41:38 -0300

    feat: add logic to show response on form component

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ApiService } from './api.service';
+import { map } from 'rxjs';
+import { Message } from './interfaces/api-response.interface';
 
 @Component({
   selector: 'app-form',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrl: './form.component.css',
 })
 export class FormComponent {
+  apiService: ApiService = inject(ApiService);
+  apiResponse?: string;
+  apiResponseError?: string;
 
+  constructor() {
+    this.apiService
+      .getResponse('Qué es angular')
+
+      .subscribe({
+        next: (value) => {
+          this.apiResponse = value.content;
+        },
+        error: (err) => {
+          console.log({ ERROR: err.error.error.message });
+          this.apiResponseError = err.error.error.message;
+        },
+      });
+  }
 }
