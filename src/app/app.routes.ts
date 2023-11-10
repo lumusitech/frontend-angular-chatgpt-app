3b0e9ae635bb19ab6c902b5af484cd2ddb4bb1a7
lumusitech <lumusitech@gmail.com>
2023-11-10 00:37:19 -0300

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'form',
+    pathMatch: 'full',
+  },
+  {
+    path: 'form',
+    loadComponent: () =>
+      import('./components/form/form.component').then((m) => m.FormComponent),
+    title: 'Form',
+  },
+];
